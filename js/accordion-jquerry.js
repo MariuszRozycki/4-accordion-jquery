89c62f531e139386225e4400d95e217ee23e5436
mariansc83 <mariansc83@icloud.com>
2020-10-25 21:12:48 +0100

    Add 4-accordion-jquery

---
+++ js/accordion-jquerry.js
@@ -0,0 +1,13 @@
+console.log('Warsztaty Accordion');
+
+$(document).ready(function () {
+  $('.accordion-item').click(function () {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $(this).find('.accordion-content').slideUp();
+    } else {
+      $(this).addClass('open');
+      $(this).find('.accordion-content').slideDown();
+    }
+  });
+});
